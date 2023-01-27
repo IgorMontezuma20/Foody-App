6e6b01f6cc993a8029203eb274bd219d0f919641
IgorMontezuma20 <igorminranda17@gmail.com>
2023-01-27 12:54:00 -0300

    RecipesTypeConverter created

---
+++ app/src/main/java/com/example/foody/RecipesTypeConverter.kt
@@ -0,0 +1,24 @@
+package com.example.foody
+
+import androidx.room.TypeConverter
+import com.example.foody.models.FoodRecipe
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    //Convertendo ao objeto para string
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String{
+        return gson.toJson(foodRecipe)
+    }
+
+    //Convertendo a string para objeto
+    fun stringToFoodRecipe(data: String): FoodRecipe{
+        val listType = object : TypeToken<FoodRecipe>(){}.type
+        return gson.fromJson(data, listType)
+    }
+
+}
