8b8a14508773c07e35fab7c0d98291a5e8301911
IgorMontezuma20 <igorminranda17@gmail.com>
2023-01-28 13:45:49 -0300

    LocalDataSource created and injected

---
+++ app/src/main/java/com/example/foody/data/LocalDataSource.kt
@@ -0,0 +1,20 @@
+package com.example.foody.data
+
+import com.example.foody.di.database.RecipesDao
+import com.example.foody.di.database.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao: RecipesDao
+) {
+
+    fun redDatabase(): Flow<List<RecipesEntity>> {
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
+        recipesDao.insertRecipes(recipesEntity)
+    }
+
+}
