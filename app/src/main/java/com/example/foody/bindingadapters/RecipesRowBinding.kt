265c652ec52e08b42c01bf74c350b20cfd3da870
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-05 11:12:59 -0300

    sending resul to DetailActivity

--- app/src/main/java/com/example/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foody/bindingadapters/RecipesRowBinding.kt
@@ -1,21 +1,40 @@
 package com.example.foody.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.foody.R
 
+import com.example.foody.models.Result
+import com.example.foody.ui.fragments.recipes.RecipesFragmentDirections
+
 class RecipesRowBinding {
 
-    companion object{
+    companion object {
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
-        fun loadImageFromUrl(imageView: ImageView, imageUrl: String){
-            imageView.load(imageUrl){
+        fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
+            imageView.load(imageUrl) {
                 crossfade(600)
                 error(R.drawable.ic_error_placeholder)
             }
@@ -23,22 +42,22 @@ class RecipesRowBinding {
 
         @BindingAdapter("setNumberOfLikes")
         @JvmStatic
-        fun setNumberOfLikes(textView: TextView, likes: Int){
+        fun setNumberOfLikes(textView: TextView, likes: Int) {
             textView.text = likes.toString()
         }
 
         @BindingAdapter("setNumberOfMinutes")
         @JvmStatic
-        fun setNumberOfMinutes(textView: TextView, minutes: Int){
+        fun setNumberOfMinutes(textView: TextView, minutes: Int) {
             textView.text = minutes.toString()
         }
 
         @BindingAdapter("applyVeganColor")
         @JvmStatic
-        fun applyVeganColor(view: View, vegan: Boolean){
-            if(vegan){
-                when(view){
-                    is TextView ->{
+        fun applyVeganColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                when (view) {
+                    is TextView -> {
                         view.setTextColor(
                             ContextCompat.getColor(
                                 view.context,
@@ -46,7 +65,7 @@ class RecipesRowBinding {
                             )
                         )
                     }
-                    is ImageView ->{
+                    is ImageView -> {
                         view.setColorFilter(
                             ContextCompat.getColor(
                                 view.context,
