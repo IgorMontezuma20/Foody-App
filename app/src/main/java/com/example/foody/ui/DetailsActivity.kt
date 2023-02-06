1c29aec3e0d639fde8591d2dfe6438071a6f259b
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-06 20:24:24 -0300

    PagerAdapter Class created

--- app/src/main/java/com/example/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/foody/ui/DetailsActivity.kt
@@ -2,12 +2,17 @@ package com.example.foody.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.navArgs
 import com.example.foody.R
+import com.example.foody.adapters.PagerAdapter
 import com.example.foody.databinding.ActivityDetailsBinding
+import com.example.foody.ui.fragments.ingridients.IngridientsFragment
+import com.example.foody.ui.fragments.instructions.InstructionsFragment
+import com.example.foody.ui.fragments.overview.OverviewFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -22,7 +27,32 @@ class DetailsActivity : AppCompatActivity() {
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngridientsFragment())
+        fragments.add(InstructionsFragment())
 
+        val titles = ArrayList<String>()
+        titles.add("Visão geral")
+        titles.add("Ingredientes")
+        titles.add("Instruções")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            this
+        )
+
+        binding.viewpager2.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewpager2) {tab, position ->
+            tab.text = titles[position]
+        }.attach()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
