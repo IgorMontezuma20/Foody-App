693a4141f1ae4ccff16d24102b60fe7e1e72bc28
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-08 13:31:48 -0300

    adding webView in IntructionsFragment

--- app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foody.R
+import com.example.foody.databinding.FragmentInstructionsBinding
+import com.example.foody.models.Result
+import com.example.foody.util.Constants
 
 class InstructionsFragment : Fragment() {
 
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val webSiteUrl: String = myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(webSiteUrl)
+
+        return binding.root
     }
 
 }
