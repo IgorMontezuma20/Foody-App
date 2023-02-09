6c66d0e1982bc64e2d00d0e321cecc0fa37d321f
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-09 11:42:43 -0300

    Modifying RecipesTypeConverter class

--- app/src/main/java/com/example/foody/data/database/RecipesTypeConverter.kt
+++ app/src/main/java/com/example/foody/data/database/RecipesTypeConverter.kt
@@ -4,6 +4,7 @@ import androidx.room.TypeConverter
 import com.example.foody.models.FoodRecipe
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.example.foody.models.Result
 
 class RecipesTypeConverter {
 
@@ -17,8 +18,19 @@ class RecipesTypeConverter {
 
     //Convertendo a string para objeto
     @TypeConverter
-    fun stringToFoodRecipe(data: String): FoodRecipe{
-        val listType = object : TypeToken<FoodRecipe>(){}.type
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+    @TypeConverter
+    fun resultToString(result: Result): String {
+        return gson.toJson(result)
+    }
+
+    @TypeConverter
+    fun stringToResult(data: String): Result {
+        val listType = object : TypeToken<Result>() {}.type
         return gson.fromJson(data, listType)
     }
 
